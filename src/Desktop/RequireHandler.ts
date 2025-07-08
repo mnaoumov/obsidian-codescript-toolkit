8ff781310134f8c4743606be6bfe95166740a461
Michael Naumov <mnaoumov@gmail.com>
2025-07-08 14:34:19 -0600

    Make markdown module support typescript syntax

--- src/Desktop/RequireHandler.ts
+++ src/Desktop/RequireHandler.ts
@@ -27,6 +27,7 @@ import { CacheInvalidationMode } from '../CacheInvalidationMode.ts';
 import {
   ENTRY_POINT,
   extractCodeScript,
+  getCodeScriptName,
   getModuleTypeFromPath,
   MODULE_NAME_SEPARATOR,
   NODE_MODULES_FOLDER,
@@ -310,7 +311,8 @@ Consider using cacheInvalidationMode=${CacheInvalidationMode.Never} or ${this.ge
   private requireMd(path: string): unknown {
     const md = this.readFile(path);
     const code = extractCodeScript(md, path);
-    return this.requireString(code, path);
+    const codeScriptName = getCodeScriptName(path) || '(default)';
+    return this.requireString(code, `${path}.code-script.${codeScriptName}.ts`);
   }
 
   private requireModule(moduleName: string, parentFolder: string, cacheInvalidationMode: CacheInvalidationMode, moduleType?: ModuleType): unknown {
