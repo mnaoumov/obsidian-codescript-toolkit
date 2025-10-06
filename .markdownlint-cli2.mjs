bf6fd9e1d905059943322ce37ee1e2aa9ba55d3e
Michael Naumov <mnaoumov@gmail.com>
2025-10-05 18:57:36 -0600

    chore: enable markdownlint

--- .markdownlint-cli2.mjs
+++ .markdownlint-cli2.mjs
@@ -1,7 +1,5 @@
-const config = {
-  config: {
-    MD013: false
-  }
-};
-
+import { createJiti } from 'jiti';
+const jiti = createJiti(import.meta.url);
+const { config } = await jiti.import('./.markdownlint-cli2.mts');
+// eslint-disable-next-line import-x/no-default-export -- markdownlint-cli2 infrastructure requires a default export.
 export default config;
