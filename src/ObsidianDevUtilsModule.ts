4f3cadada93fda1f081d583cf09c02e1138f6f83
Michael Naumov <mnaoumov@gmail.com>
2026-01-08 22:22:57 -0600

    fix: exclude @types folder

--- src/ObsidianDevUtilsModule.ts
+++ src/ObsidianDevUtilsModule.ts
@@ -8,27 +8,29 @@ import type { RequireOptions } from './types.ts';
 // eslint-disable-next-line import-x/no-relative-packages -- package.json is not exported.
 import packageJson from '../node_modules/obsidian-dev-utils/package.json' with { type: 'json' };
 
-const LIB_NAME = 'obsidian-dev-utils';
-const FORBIDDEN_EXPORT_PATH = './ScriptUtils';
-const WILDCARD_EXPORT_PATH = '*';
-const PROPERTY_PATH_SEPARATOR = '.';
 const EXPORT_PATH_PREFIX = './';
-const ROOT_EXPORT_PATH = '.';
 const EXPORT_PATH_SEPARATOR = '/';
+const FORBIDDEN_EXPORT_PATHS = ['ScriptUtils', '@types'];
+const LIB_NAME = 'obsidian-dev-utils';
+const PROPERTY_PATH_SEPARATOR = '.';
+const ROOT_EXPORT_PATH = '.';
+const WILDCARD_EXPORT_PATH = '*';
 
 export async function registerObsidianDevUtilsModule(specialModuleFactories: Map<string, (options: Partial<RequireOptions>) => unknown>): Promise<void> {
   specialModuleFactories.set(LIB_NAME, () => obsidianDevUtils);
 
   for (const exportPath of Object.keys(packageJson.exports)) {
-    if (exportPath.startsWith(FORBIDDEN_EXPORT_PATH)) {
-      continue;
-    }
-
     if (exportPath.endsWith(WILDCARD_EXPORT_PATH)) {
       continue;
     }
 
     const relativeExportPath = trimStart(exportPath, EXPORT_PATH_PREFIX);
+
+    const pathParts = relativeExportPath.split(EXPORT_PATH_SEPARATOR);
+    if (pathParts.some((pathPart) => FORBIDDEN_EXPORT_PATHS.includes(pathPart))) {
+      continue;
+    }
+
     const propertyPath = relativeExportPath.replaceAll(EXPORT_PATH_SEPARATOR, PROPERTY_PATH_SEPARATOR);
 
     const module = relativeExportPath === ROOT_EXPORT_PATH ? obsidianDevUtils : getNestedPropertyValue(obsidianDevUtils, propertyPath) as object;
